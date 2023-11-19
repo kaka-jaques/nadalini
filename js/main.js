0c2aaea4536f33305b5ed7df1b38da6bf3bcafaa
Kalil J. Fakhouri <53912803+kaka-jaques@users.noreply.github.com>
2023-11-19 00:15:04 -0300

    UPDATE
    INTRO STARTED
    SOCIAL STARTED
    PROMO STARTED
    SWIPERS CONFIG
    HEADER ANIMATION
    FOOTER STARTED
    MYSQLJS LIB ADDED

---
+++ js/main.js
@@ -0,0 +1,60 @@
+//SWIPERS
+
+const swiper = new Swiper('.introSwiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    effect: "fade",
+    centeredSlides: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+const swiper2 = new Swiper('.socialSwiper', {
+    slidesPerView: 5,
+    freeMode: true,
+    grid: {
+        rows: 2
+    },
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    }
+})
+
+const swiper3 = new Swiper('.promoSwiper', {
+    slidesPerView: 4,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    freeMode: true,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+})
+
+//HEADER SIMPLIFY FUNCTION
+
+$(window).scroll(function(){
+    if($(window).scrollTop() >= 100){
+        document.querySelector("#navbar-menu").classList.add("simplified")
+    }
+    else {
+        document.querySelector("#navbar-menu").classList.remove("simplified")
+    }
+})
